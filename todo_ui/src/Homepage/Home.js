a8f85734ee34a9277f73d0c11e527800f75d77bb
Magnús Friðrik <magnusf24@gmail.com>
2024-04-19 10:45:49 +0000

    Setup a React environment

---
+++ todo_ui/src/Homepage/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Todo App</h1>
+            <p>Click on the Todos link above to view your todos.</p>
+        </div>
+    );
+    }
+
+export default Home;
