899667f1352a8067ccb6cf73f23b6aede066d6e4
Magnús Friðrik <magnusf24@gmail.com>
2024-06-03 20:24:05 +0000

    Added an input field and an 'add' button to add tasks

--- todo_ui/src/Listpage/ListCenter.js
+++ todo_ui/src/Listpage/ListCenter.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import './ListCenter.css';
 
 function ListCenter() {
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const handleInputChange = (e) => {
+        setTask(e.target.value);
+    };
+
+    const handleAddTask = () => {
+        if (task.trim()) {
+            setTasks([...tasks, task]);
+            setTask("");
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleAddTask();
+        }
+    };
+
     return (
-        <div>
-            <h1>List Center</h1>
+        <div className="container">
+            <div className="inputContainer">
+                <input
+                    type="text"
+                    value={task}
+                    onChange={handleInputChange}
+                    onKeyPress={handleKeyPress}
+                    placeholder="Enter a task"
+                />
+                <button onClick={handleAddTask}>Add</button>
+            </div>
+            <ul>
+                {tasks.map((t, index) => (
+                    <li key={index}>{t}</li>
+                ))}
+            </ul>
         </div>
     );
 }
